4f2593768bdb8fc7ce7fb419cd7519debc3ddb51
DeoEsor <RulianValeriy@yandex.ru>
2023-01-13 21:50:43 +0000

    added src from template

---
+++ src/components/layout/partials/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+  return (
+    <div>
+      <h1>
+        <Link to="/"> 
+             Gefest Vision
+        </Link>
+      </h1>
+    </div>
+  );
+}
+
+export default Logo;
